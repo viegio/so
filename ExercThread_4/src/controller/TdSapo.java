a2533f5805eed0c5326bf563c6b54e792cdf1b77
Giovanna <Giovanna@DESKTOP-N5BF7R5>
2022-09-14 14:31:00 -0300

    Semáforo Commit

--- ExercThread_4/src/controller/TdSapo.java
+++ ExercThread_4/src/controller/TdSapo.java
@@ -1,22 +1,52 @@
 package controller;
+import java.util.concurrent.Semaphore;
 
 import view.Main;
 
 public class TdSapo extends Thread {
+	
+	private Semaphore semaforo;
 	private int i;
 	
 	//construtor
-	public TdSapo(int i) {
+	public TdSapo(int i, Semaphore semaforo) {
 		this.i = i;
+		this.semaforo = semaforo;
 	}
 	
 	//executar
 	@Override
 	public void run() {
-		double pulo=0.0, dist=10.0, percorrido=0;
+		pulo();
 		
+//		chegada();
+		try {
+			semaforo.acquire();
+			chegada();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+		
+//		podio();
+		try {
+			semaforo.acquire();
+			podio();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+		
+	}
+
+	private void pulo() {
+		double pulo=0.0, dist=1000.0, percorrido=0;
+				
 		while (percorrido<dist) {
-			pulo = (Math.random() * 10) % 2;
+			pulo = (Math.random() * 10) + 1;
 			percorrido = percorrido + pulo;
 			
 			if (percorrido > dist) {
@@ -24,10 +54,32 @@ public class TdSapo extends Thread {
 				percorrido = percorrido - (percorrido - dist);
 			}
 			
-			//pulo = Double.parseDouble(df.format(pulo));
-			System.out.println("Sapo"+(i+1) + " ==> "+ "Pulo: "+pulo + " Percorrido: " + percorrido);
+			try {
+				semaforo.acquire();
+				//pulo = Double.parseDouble(df.format(pulo));
+				System.out.println("Sapo #"+(i+1) + " ==> "+ "Pulo: "+pulo + " Percorrido: " + percorrido);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				semaforo.release();
+			}
+			
 		}
-		System.out.println("Sapo"+(i+1)+" Chegou ao final! " + (Main.fim+1) + "º lugar!");
+	}
+	
+	private void chegada() {
+		System.out.println("Sapo #"+(i+1)+" Chegou ao final! " + (Main.fim+1) + "º lugar!");
+		Main.podio[Main.fim] = "Sapo #" + (i+1);
 		Main.fim = Main.fim + 1;
+	}
+	
+	private void podio() {
+		if (Main.podio[4] != null) {
+			System.out.println("-------------------------------PÓDIO-------------------------------");
+			for (int p=0; p<5; p++) {
+				System.out.println(p+1 + "º lugar: " + Main.podio[p]);
+			}
+		}
 	}
 }
--- ExercThread_4/src/view/Main.java
+++ ExercThread_4/src/view/Main.java
@@ -1,4 +1,6 @@
 package view;
+import java.util.concurrent.Semaphore;
+
 import controller.TdSapo;
 
 public class Main {
@@ -6,13 +8,14 @@ public class Main {
 	public static String podio[] = new String[5];
 	
 	public static void main(String[] args) {
+		Semaphore semaforo = new Semaphore(1);
 		int i;
 		fim = 0;
 		
-		System.out.println("CORRIDA DE SAPOS\nPULO MÁXIMO: 1.9M - DISTÂNCIA: 10M\nCOMEÇAR!");
+		System.out.println("CORRIDA DE SAPOS\nPULO MÁXIMO: 10M - DISTÂNCIA: 1000M\nCOMEÇAR!");
 		//iniciar threads
 		for (i=0;i<5;i++) {
-			TdSapo sapo = new TdSapo(i);
+			TdSapo sapo = new TdSapo(i, semaforo);
 			sapo.start();
 		}
 	}
